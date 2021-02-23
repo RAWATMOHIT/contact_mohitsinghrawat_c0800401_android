9b3fd4d5c82cb90e65fabe3293e82c1ee0d881f0
hp <rawatved04@gmail.com>
2021-02-24 03:46:38 +0530

    Initial commit

---
+++ app/src/test/java/contact_mohitsinghrawat_c0800401_android/com/contactslist/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package contact_mohitsinghrawat_c0800401_android.com.contactslist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
